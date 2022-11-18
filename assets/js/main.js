936fa99f58bbb6e918bd976a1bfbae58bdf95094
Caio Cabral <ocaiocabral@gmail.com>
2022-11-18 18:18:29 -0400

    Header icons gap adjustement

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,10 @@
 const header = document.querySelector(".header");
 const burger = document.querySelector(".burger");
 
-const headerHeight = header.offsetHeight;
-burger.style.height = `${headerHeight*0.4}px`;
-burger.style.width = `${headerHeight*0.4}px`;
+function resizeBurger() {
+   const headerHeight = header.offsetHeight;
+   burger.style.height = `${headerHeight*0.4}px`;
+   burger.style.width = `${headerHeight*0.4}px`;
+}
+window.addEventListener("load", resizeBurger);
+window.addEventListener("resize", resizeBurger);
